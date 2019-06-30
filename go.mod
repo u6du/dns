00e04884eba82a24f5a6b03f94dbea53b256f6ba
- <->
2019-06-30 16:10:26 +0800

    .

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module dns
 
 go 1.12
+
+require github.com/u6du/ex v0.0.0-20190628070729-e33da411a5b7
--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+
+	"github.com/u6du/ex"
+)
 
 func main() {
+	dnsServer := "8.8.8.8"
+	c, err := net.Dial("udp", dnsServer)
+	ex.Panic(err)
+
 	fmt.Println("vim-go")
 }
