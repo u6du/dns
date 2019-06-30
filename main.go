aae2967090c874194e65ce0b69e510c67fe4de44
- <->
2019-06-30 17:47:19 +0800

    .

--- main.go
+++ main.go
@@ -4,14 +4,13 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"net"
-
-	"github.com/u6du/ex"
+	"sync"
+	"time"
 )
 
 
-func LookupTXT(nameserver string){
+func LookupTXT(host,nameserver string) string{
 	resolve := &net.Resolver{
 		PreferGo: true,
 		Dial: func(context context.Context, _, address string) (net.Conn, error) {
@@ -25,14 +24,124 @@ func LookupTXT(nameserver string){
 			return conn, nil
 		},
 	}
-	li, err := resolve.LookupTXT(context.Background(), "ip4.6du.host")
-	ex.Panic(err)
-	for i := range li {
-		fmt.Printf("%s %s\n\n", nameserver, li[i])
+	li, err := resolve.LookupTXT(context.Background(), host)
+	if err == nil{
+		for i := range li {
+			txt := li[i]
+			if len(txt)>0{
+				return txt
+			}
+		}
+	}
+
+	return ""
+
+}
+
+/*
+百度公共DNS, "指引"未来
+  180.76.76.76
+  2400:da00::6666
+
+  阿里云DNS
+  223.5.5.5
+
+  dnspod
+  119.29.29.29
+
+  cernet 的官方 DNS
+  101.7.8.9
+
+  下一代互联网北京研究中心
+  240C::6666
+
+  CNNIC IPv6 DNS 服务器
+  2001:dc7:1000::1
+
+  清华大学 TUNA 协会 IPv6 DNS 服务器
+  2001:da8::666
+
+  cloudflare
+  1.1.1.1
+  2606:4700:4700::1111
+
+  yandex
+  77.88.8.8
+  2a02:6b8::feed:0ff
+
+  IBM
+  9.9.9.9
+  2620:fe::fe
+
+  Google DNS
+  8.8.8.8
+  2001:4860:4860::8888
+ */
+
+var Ipv4 = []string {
+	"77.88.8.8:53", // yandex
+	"9.9.9.9:53", // IBM
+	"8.8.8.8:53", // Google
+}
+
+
+var Ipv6 = []string{
+	"2a02:6b8::feed:0ff/53", //yandex
+	"2001:4860:4860::8888/53", // Google
+	"2620:fe::fe/53", // IBM
+}
+
+
+func ResolveTxt(host string, nameserver []string) string{
+	if len(nameserver) == 0{
+		return ""
+	}
+
+	ch := make(chan string)
+
+	for i:=range nameserver{
+		go func(server string) {
+			txt := LookupTXT(host,server)
+			println("nameserver ", server, " : ", txt)
+			ch <- txt
+		}(nameserver[i])
+	}
+
+	total := 1
+
+	for {
+		txt:= <-ch
+		//if len(txt) > 0 || total>=len(nameserver) {
+		if total>=len(nameserver) {
+			return txt
+		}
+		total++
 	}
 }
 
+
 func main() {
-	LookupTXT("8.8.8.8:53")
-	LookupTXT("[2001:4860:4860::8888]:53")
+	host:="ip4.6du.host"
+
+	wg := sync.WaitGroup{}
+
+	run := func(nameserver []string) string{
+		wg.Add(1)
+		ch := make(chan string)
+		go func() {
+			txt:=ResolveTxt(host, nameserver)
+			ch<-txt
+			wg.Done()
+		}()
+		txt:= <-ch
+		return txt
+	}
+
+	v6txt := run(Ipv6)
+	v4txt := run(Ipv4)
+
+	wg.Wait()
+	println("v4", v4txt)
+	println("v6", v6txt)
+	time.Sleep(3*time.Second)
 }
