12e37cda39cffcee956c83f61e0918af2bea3b0d
- <->
2019-06-30 20:39:51 +0800

    .

--- main.go
+++ main.go
@@ -9,6 +9,11 @@ import (
 	"time"
 )
 
+// TODO 先用UDP DNS找
+// 找不到
+// 		用DOT找DOT找的时候容忍超时
+// 		用 t.cn 测试下是不是支持 ipv6
+
 func LookupTXT(host, nameserver string) string {
 
 	resolve := &net.Resolver{
@@ -40,21 +45,21 @@ func LookupTXT(host, nameserver string) string {
 }
 
 var Ipv4 = []string{
-	"180.76.76.76:53",     // 百度
-	"223.5.5.5:53",        // 阿里云
-	"119.29.29.29:53",     // dnspod
-	"1.0.0.1:53",          // cloudflare
-	"77.88.8.8:53",        // yandex
-	"9.9.9.9:53",          // IBM
-	"8.8.8.8:53",          // Google
-	"101.7.8.9:53",        // cernet 的官方 DNS
-	"208.67.222.222:443",  // OPENDNS
-	"114.114.114.114:53",  // 114 dns
+	"180.76.76.76:53",      // 百度
+	"223.5.5.5:53",         // 阿里云
+	"119.29.29.29:53",      // dnspod
+	"1.0.0.1:53",           // cloudflare
+	"77.88.8.8:53",         // yandex
+	"9.9.9.9:53",           // IBM
+	"8.8.8.8:53",           // Google
+	"101.7.8.9:53",         // cernet 的官方 DNS
+	"208.67.222.222:443",   // OPENDNS
+	"114.114.114.114:53",   // 114 dns
 	"176.103.130.130:5353", // AdGuard DNS
 }
 
 var Ipv6 = []string{
-	"[2a00:5a60::ad1:0ff]:5353", // AdGuard DNS 
+	"[2a00:5a60::ad1:0ff]:5353", // AdGuard DNS
 	"[2620:0:ccc::2]:443",       // OPENDNS
 	"[2400:da00::6666]:53",      // 百度
 	"[2001:da8:202:10::36]:53",  // 北京邮电大学 IPv6 DNS 服务器
@@ -65,7 +70,7 @@ var Ipv6 = []string{
 	"[2620:fe::fe]:53",          // IBM
 }
 
-func ResolveTxt(host string, nameserver []string) string {
+func ResolveTxt(host string, nameserver []string, verify func(string) bool) string {
 	if len(nameserver) == 0 {
 		return ""
 	}
@@ -75,9 +80,7 @@ func ResolveTxt(host string, nameserver []string) string {
 	for i := range nameserver {
 		go func(server string) {
 			txt := LookupTXT(host, server)
-			if len(txt) == 0 {
-				println("nameserver ", server, " : ", txt)
-			}
+			println("nameserver ", server, " : ", txt)
 
 			ch <- txt
 		}(nameserver[i])
@@ -89,7 +92,7 @@ func ResolveTxt(host string, nameserver []string) string {
 		txt := <-ch
 		// TODO 验证签名和时间
 		//if len(txt) > 0 || total>=len(nameserver) {
-		if total >= len(nameserver) {
+		if verify(txt) || total >= len(nameserver) {
 			return txt
 		}
 		total++
@@ -106,7 +109,9 @@ func main() {
 	run := func(nameserver []string, out *string) {
 		wg.Add(1)
 		go func() {
-			*out = ResolveTxt(host, nameserver)
+			*out = ResolveTxt(host, nameserver, func(txt string) bool {
+				return len(txt) > 0
+			})
 			wg.Done()
 		}()
 	}
